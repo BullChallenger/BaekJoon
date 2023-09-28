f01c5ee864ed98c6ecabc8a0a04bf997e79b7822
BullChallenger <am23am45@naver.com>
2023-09-28 17:09:06 +0900

    특정 문자와의 최소 거리 구하기 오른쪽 방향으로 거리 구했을 때와 왼쪽 방향으로 거리 구했을 때를 비교하여 해결

--- src/Study/WordDistance.java
+++ src/Study/WordDistance.java
@@ -14,54 +14,30 @@ public class WordDistance {
         String input = input();
         String sentence = input.split(" ")[0];
         char target = input.split(" ")[1].charAt(0);
+        int[] answer = new int[sentence.length()];
+        int pointer = sentence.length() + 1;
 
         for (int i = 0; i < sentence.length(); i++) {
-            if (sentence.charAt(i) == target) System.out.print(0 + " ");
-            else {
-                if (i - 1 < 0) {
-                    int rt = i + 1;
-                    while (rt < sentence.length()) {
-                        if (sentence.charAt(rt) == target) {
-                            System.out.print(rt - i + " ");
-                            break;
-                        }
-                        else rt++;
-                    }
-                } else if (i + 1 == sentence.length()) {
-                    int lt = i - 1;
-                    while (lt > 0) {
-                        if (sentence.charAt(lt) == target) {
-                            System.out.print(i - lt + " ");
-                            break;
-                        }
-                        else lt--;
-                    }
-                } else {
-                    int rt = i + 1;
-                    int rtDistance = Integer.MAX_VALUE;
-                    int lt = i - 1;
-                    int ltDistance = Integer.MAX_VALUE;
-                    while (rt < sentence.length()) {
-                        if (sentence.charAt(rt) == target) {
-                            rtDistance = rt - i;
-                            break;
-                        }
-                        else rt++;
-                    }
-                    while (lt >= 0) {
-                        if (sentence.charAt(lt) == target) {
-                            ltDistance = i - lt;
-                            break;
-                        }
-                        else lt--;
-                    }
-                    if (rtDistance > ltDistance) {
-                        System.out.print(ltDistance + " ");
-                    } else {
-                        System.out.print(rtDistance + " ");
-                    }
-                }
+            if (sentence.charAt(i) == target) {
+                pointer = 0;
+            } else {
+                pointer++;
             }
+            answer[i] = pointer;
+        }
+
+        pointer = sentence.length() + 1;
+        for (int i = sentence.length() - 1; i >= 0; i--) {
+            if (sentence.charAt(i) == target) {
+                pointer = 0;
+            } else {
+                pointer++;
+                answer[i] = Math.min(answer[i], pointer);
+            }
+        }
+
+        for (int i : answer) {
+            System.out.print(i + " ");
         }
     }
 
