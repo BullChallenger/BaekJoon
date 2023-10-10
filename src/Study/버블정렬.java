8217edea036f7cd85a1bb3c4ae167906d0d3a7da
BullChallenger <am23am45@naver.com>
2023-10-10 16:23:26 +0900

    버블정렬
    clear

---
+++ src/Study/버블정렬.java
@@ -0,0 +1,36 @@
+package Study;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 버블정렬 {
+
+    public static void main(String[] args) {
+        solution();
+    }
+
+    private static void solution() {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+}
