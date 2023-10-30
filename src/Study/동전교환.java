84503a178a70a48832fd16cc09f937696087ae2c
BullChallenger <am23am45@naver.com>
2023-10-30 16:42:01 +0900

    동전교환
    TimeLimit으로 인한 코드 변경
    
    입력 값을 내림차순 정렬하여 큰 값부터 진행, 기준 값을 높여둠으로써 시간을 단축시킬 수 있다

--- src/Study/동전교환.java
+++ src/Study/동전교환.java
@@ -1,11 +1,13 @@
 package Study;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class 동전교환 {
 
     private static int N, M;
-    private static int[] coins;
+    private static Integer[] coins;
     private static int result = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
@@ -15,10 +17,11 @@ public class 동전교환 {
     private static void solution() {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
-        coins = new int[N];
+        coins = new Integer[N];
         for (int i = 0; i < N; i++) {
             coins[i] = sc.nextInt();
         }
+        Arrays.sort(coins, Collections.reverseOrder());
         M = sc.nextInt();
 
         DFS(0,  0);
@@ -27,6 +30,7 @@ public class 동전교환 {
 
     private static void DFS(int sum, int L) {
         if (sum > M) return;
+        if (L > result) return;
         if (sum == M) {
             result = Math.min(L, result);
             return;
